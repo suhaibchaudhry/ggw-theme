f013cb534d54cade9d529160ca8a62915bff8c27
Asad Hasan <asadpakistani@gmail.com>
2014-06-10 01:54:50 -0500

    Static company tobacco id added, and dynamic tobacco id moved.

--- ggw_backend/uc_order-customer.tpl.php
+++ ggw_backend/uc_order-customer.tpl.php
@@ -44,12 +44,13 @@
         <p><span class="label">Email: </span>info@general-goods.com</p>
         <p><span class="label">Website: </span>info@general-goods.com</p>
         <p><span class="label">Phone: </span>713-780-3636 <span class="label">Fax: </span>713-780-1718</p>
-        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
+        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span>93044639</p><?php endif; ?>
       </div>
       <div class="invoice-info column">
         <!--<p class="page"><span class="label">Page:</span> <span class="value"></span></p>-->
         <p class="invoiceid"><span class="label">Invoice #:</span> <span class="value"><?php echo $order_link; ?></span></p>
         <p class="invoicedate"><span class="label">Invoice Date:</span> <span class="value"><?php echo date("n/j/Y g:ia", $order->created); ?></span></p>
+        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
       </div>
     </div>
 
