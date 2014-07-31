49ac13e064d3c6090ed4c85363294a77ea29c9d4
Asad Hasan <asadpakistani@gmail.com>
2014-07-31 12:27:39 -0500

    Adjust invoice for dp

--- ggw_backend/uc_order-customer.tpl.php
+++ ggw_backend/uc_order-customer.tpl.php
@@ -3,6 +3,8 @@
    * @file
    * This file is the default customer invoice template for Ubercart.
    */
+    //db_query("SELECT billing_zone FROM uc_orders WHERE order_id = '%d'", $order->order_id);
+
     $uid = $order->uid;
     if($uid) {
       $profile = content_profile_load('profile', $uid);
@@ -23,7 +25,11 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="<?php print base_path() . path_to_theme(); ?>/css/invoice.css" media="all" />
+  <?php if($order->billing_zone == '1') { ?>
+  <title>Invoice</title>
+  <?php } else { ?>
   <title><?php print $profile->title ?> (<?php print $profile->name ?>)</title>
+  <?php } ?>
   <style type="text/css" media="all">
   body {
     -webkit-print-color-adjust:exact;
@@ -47,52 +53,54 @@
 </head>
 <body>
   <div class="invoice">
-    <div class="header">
-      <div class="logo column"></div>
-      <div class="address column">
-        <p>8000 Harwin dr. Suite #200 Houston, TX 77036</p>
-        <p><span class="label">Email: </span>info@general-goods.com</p>
-        <p><span class="label">Website: </span>info@general-goods.com</p>
-        <p><span class="label">Phone: </span>713-780-3636 <span class="label">Fax: </span>713-780-1718</p>
-        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span>93044639</p><?php endif; ?>
+    <?php if($order->billing_zone == '0') { ?>
+      <div class="header">
+        <div class="logo column"></div>
+        <div class="address column">
+          <p>8000 Harwin dr. Suite #200 Houston, TX 77036</p>
+          <p><span class="label">Email: </span>info@general-goods.com</p>
+          <p><span class="label">Website: </span>info@general-goods.com</p>
+          <p><span class="label">Phone: </span>713-780-3636 <span class="label">Fax: </span>713-780-1718</p>
+          <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span>93044639</p><?php endif; ?>
+        </div>
+        <div class="invoice-info column">
+          <?php if($status_names[$order_status]) : ?><p class="invoiceid" style="font-size: 1.5em;"><span class="label"><?php print $status_names[$order_status] ?></span></p><?php endif; ?>
+          <p class="invoiceid"><span class="label">Invoice #:</span> <span class="value"><?php echo $order_link; ?></span></p>
+          <p class="invoicedate"><span class="label">Invoice Date:</span> <span class="value"><?php echo date("n/j/Y g:ia", $order->created); ?></span></p>
+          <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
+        </div>
       </div>
-      <div class="invoice-info column">
-        <?php if($status_names[$order_status]) : ?><p class="invoiceid" style="font-size: 1.5em;"><span class="label"><?php print $status_names[$order_status] ?></span></p><?php endif; ?>
-        <p class="invoiceid"><span class="label">Invoice #:</span> <span class="value"><?php echo $order_link; ?></span></p>
-        <p class="invoicedate"><span class="label">Invoice Date:</span> <span class="value"><?php echo date("n/j/Y g:ia", $order->created); ?></span></p>
-        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
-      </div>
-    </div>
 
-    <div class="customer-info box">
-      <div class="header-line"></div>
-      <div class="billing-address cust-address">
-        <span class="label">Sold To:</span>
-        <span class="value">
-          <?php if($uid) { ?>
-          <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
-          <?php print $profile->field_contact_remarks[0]['value'] ?><br />
-          <?php print $profile->field_company_address[0]['street1'] ?><br />
-          <?php print $profile->field_company_address[0]['city'] ?>, <?php print $profile->field_company_address[0]['state'] ?> <?php print $profile->field_company_address[0]['zip'] ?><br />
-          <?php } else { ?>
-            Walk-in Customer
-          <?php } ?>
-        </span>
+      <div class="customer-info box">
+        <div class="header-line"></div>
+        <div class="billing-address cust-address">
+          <span class="label">Sold To:</span>
+          <span class="value">
+            <?php if($uid) { ?>
+            <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
+            <?php print $profile->field_contact_remarks[0]['value'] ?><br />
+            <?php print $profile->field_company_address[0]['street1'] ?><br />
+            <?php print $profile->field_company_address[0]['city'] ?>, <?php print $profile->field_company_address[0]['state'] ?> <?php print $profile->field_company_address[0]['zip'] ?><br />
+            <?php } else { ?>
+              Walk-in Customer
+            <?php } ?>
+          </span>
+        </div>
+        <?php if($uid) : ?>
+        <div class="shipping-address cust-address">
+          <span class="label">Deliver To:</span>
+          <span class="value">
+            <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
+            <?php print $profile->field_shipping_address[0]['value'] ?><br />
+            <?php print $profile->field_shipping_address[0]['street1'] ?><br />
+            <?php print $profile->field_shipping_address[0]['city'] ?>, <?php print $profile->field_shipping_address [0]['state'] ?> <?php print $profile->field_shipping_address [0]['zip'] ?><br />
+            <?php print $profile->field_contact_remarks[0]['value'] ?><br /><br />
+            <strong>Phone: </strong><?php print $profile->field_profile_company_phone[0]['number'] ?>
+          </span>
+        </div>
+        <?php endif; ?>
       </div>
-      <?php if($uid) : ?>
-      <div class="shipping-address cust-address">
-        <span class="label">Deliver To:</span>
-        <span class="value">
-          <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
-          <?php print $profile->field_shipping_address[0]['value'] ?><br />
-          <?php print $profile->field_shipping_address[0]['street1'] ?><br />
-          <?php print $profile->field_shipping_address[0]['city'] ?>, <?php print $profile->field_shipping_address [0]['state'] ?> <?php print $profile->field_shipping_address [0]['zip'] ?><br />
-          <?php print $profile->field_contact_remarks[0]['value'] ?><br /><br />
-          <strong>Phone: </strong><?php print $profile->field_profile_company_phone[0]['number'] ?>
-        </span>
-      </div>
-      <?php endif; ?>
-    </div>
+    <?php } ?>
 
     <div class="products">
       <?php
