75c8406e67c0ffe4021baaeeef0c2f70e840b699
Asad Hasan <asad@dadesigners.com>
2015-03-04 03:13:21 -0600

    Update user invoice.

--- ggw/template.php
+++ ggw/template.php
@@ -119,6 +119,11 @@ function ggw_preprocess_node(&$vars) {
 	}
 }
 
+function ggw_backend_invoice_credit_amount($ticketId) {
+  $ar_sql = "SELECT credit_amount FROM user_term_credits_usages u WHERE u.order_id = '%d'";
+  return db_result(db_query($ar_sql, $ticketId));
+}
+
 function ggw_backend_transaction_details($ticketId) {
   $content = '<table>';
 
@@ -129,11 +134,11 @@ function ggw_backend_transaction_details($ticketId) {
     $content .= '<tr><td>CASH</td><td>'.uc_currency_format($cash->amount_paid).'</td><td>'.uc_currency_format($cash->change).'</td></tr>';
   }
 
-  $check_sql = "SELECT ticket_id, paid_amount, cash_date FROM pos_api_expose_check_log WHERE ticket_id = '%d'";
+  $check_sql = "SELECT ticket_id, paid_amount, check_number, cash_date FROM pos_api_expose_check_log WHERE ticket_id = '%d'";
   $check = db_fetch_object(db_query($check_sql, $ticketId));
   if($check->ticket_id) {
-    $content .= '<tr><th>Method</th><th>Payment</th><th>Check Date</th></tr>';
-    $content .= '<tr><td>CHECK</td><td>'.uc_currency_format($check->paid_amount).'</td><td>'.$check->cash_date.'</td></tr>';
+    $content .= '<tr><th>Method</th><th>Payment</th><th>Check # / Check Date</th></tr>';
+    $content .= '<tr><td>CHECK</td><td>'.uc_currency_format($check->paid_amount).'</td><td>'.$check->check_number.' / '.$check->cash_date.'</td></tr>';
   }
 
   $mo_sql = "SELECT ticket_id, paid_amount, reference FROM pos_api_expose_money_order_log WHERE ticket_id = '%d'";
@@ -165,6 +170,44 @@ function ggw_backend_transaction_details($ticketId) {
     $rma_credits = db_result(db_query("SELECT IFNULL(SUM(total_refund),0) FROM {pos_api_expose_rma_refund_log} WHERE cuid = '%d' AND credit_usage_id = '0'", $rma->cuid));
     $content .= '<tr><th>Method</th><th>Amount</th><th>Current RMA Balance</th></tr>';
     $content .= '<tr><td>Redeem RMA</td><td>'.uc_currency_format($rma->total_refund).'</td><td>'.uc_currency_format($rma_credits).'</td></tr>';
+  }
+
+  $content .= '</table>';
+
+  return $content;
+}
+
+function ggw_backend_transaction_details_dp($ticketId) {
+  $content = '<table>';
+
+  $cash_sql = "SELECT ticket_id, amount_paid, `change` FROM pos_api_expose_cash_log_dp WHERE ticket_id = '%d'";
+  $cash = db_fetch_object(db_query($cash_sql, $ticketId));
+  if($cash->ticket_id) {
+    $content .= '<tr><th>Method</th><th>Payment</th><th>Change</th></tr>';
+    $content .= '<tr><td>CASH</td><td>'.uc_currency_format($cash->amount_paid).'</td><td>'.uc_currency_format($cash->change).'</td></tr>';
+  }
+
+  $check_sql = "SELECT ticket_id, paid_amount, check_number, cash_date FROM pos_api_expose_check_log_dp WHERE ticket_id = '%d'";
+  $check = db_fetch_object(db_query($check_sql, $ticketId));
+  if($check->ticket_id) {
+    $content .= '<tr><th>Method</th><th>Payment</th><th>Check # / Check Date</th></tr>';
+    $content .= '<tr><td>CHECK</td><td>'.uc_currency_format($check->paid_amount).'</td><td>'.$check->check_number.' / '.$check->cash_date.'</td></tr>';
+  }
+
+  $mo_sql = "SELECT ticket_id, paid_amount, reference FROM pos_api_expose_money_order_log_dp WHERE ticket_id = '%d'";
+  $mo = db_fetch_object(db_query($mo_sql, $ticketId));
+  if($mo->ticket_id) {
+    $content .= '<tr><th>Method</th><th>Payment</th><th>Ref.</th></tr>';
+    $content .= '<tr><td>M.O.</td><td>'.uc_currency_format($mo->paid_amount).'</td><td>'.$mo->reference.'</td></tr>';
+  }
+
+  $cc_sql = "SELECT ticket_id, amount_paid, transaction_id FROM pos_api_expose_credit_card_log_dp WHERE ticket_id = '%d'";
+  $cc = db_fetch_object(db_query($cc_sql, $ticketId));
+  if($cc->ticket_id) {
+    $content .= '<tr><th>Method</th><th>Payment</th><th>CC Type</th><th>Account</th><th>Customer Name</th></tr>';
+    $td = _pos_api_transaction_details($cc->transaction_id);
+
+    $content .= '<tr><td>Credit Card</td><td>'.uc_currency_format($cc->amount_paid).'</td><td>'.$td->xml->transaction->payment->creditCard->cardType.'</td><td>'.$td->xml->transaction->payment->creditCard->cardNumber.'</td><td>'.$td->xml->transaction->billTo->firstName.' '.$td->xml->transaction->billTo->lastName.'</td></tr>';
   }
 
   $content .= '</table>';
--- ggw/uc_order-customer.tpl.php
+++ ggw/uc_order-customer.tpl.php
@@ -3,22 +3,80 @@
    * @file
    * This file is the default customer invoice template for Ubercart.
    */
+    //db_query("SELECT billing_zone FROM uc_orders WHERE order_id = '%d'", $order->order_id);
+
     $uid = $order->uid;
     if($uid) {
       $profile = content_profile_load('profile', $uid);
       //dpm($profile);
     }
 
-    function sortCallback($a, $b) {
-      return strcasecmp($a[1], $b[1]);
+    $rma_ticket = false;
+    $title_limit = 28;
+    $packaging_limit = 8;
+    if($order_status == "Closed RMA Ticket") {
+      $rma_ticket = true;
+      $title_limit = 40;
+      $packaging_limit = 15;
+      $cash_rma = db_result(db_query("SELECT COUNT(*) FROM pos_api_expose_transaction_log WHERE ticket_id = '%d' AND rma_pid = '-1'", $order->order_id));
     }
+
+    /*$contact = '';
+    if(!empty($profile->field_profile_first_name[0]['value']) || !empty($profile->field_last_name[0]['value'])) {
+      if(!empty($profile->field_profile_first_name[0]['value'])) {
+        $contact .= $profile->field_profile_first_name[0]['value'].' ';
+      }
+
+      $contact .= $profile->field_last_name[0]['value'];
+    } else {
+      $contact = preg_replace(array('/\s*(T|t)?(X|x)?(D|d)(L|l).{0,3}(\d+|[^a-zA-Z]$)/', '/\#\d+/', '/DL: /'), '', $profile->field_contact_remarks[0]['value']);
+    }*/
+
+    function sortCallback($a, $b) {
+      return strcasecmp(trim($a[1]), trim($b[1]));
+    }
+
+    function sortCategoryCallback($a, $b) {
+      return $a['weight'] - $b['weight'];
+    }
+
+    if($cash_rma == '0') {
+      $status_names = array(
+        "Closed Ticket" => "Customer Invoice",
+        "Quote Ticket" => "Customer Quotation",
+        "Closed RMA Ticket" => "Credit Memo"
+      );
+    } else {
+      $status_names = array(
+        "Closed Ticket" => "Customer Invoice",
+        "Quote Ticket" => "Customer Quotation",
+        "Closed RMA Ticket" => "Cash Refund Receipt"
+      );
+    }
+   
+    $full_user = user_load($uid);
+
+    //if($full_user->credit_limits->pending_payments) {
+      $ticket_ar = ggw_backend_invoice_credit_amount($order->order_id);
+      if($ticket_ar) {
+        $consumption_date = db_result(db_query("SELECT utcu.consumption_date FROM {user_term_credits_usages} utcu WHERE utcu.order_id = '%d'", $order->order_id));
+        $credit = _user_term_credits_getCredits($uid, $consumption_date);
+        $payment_remaining = theme('table', array('Starting Balance', 'Ending Balance'), array(
+          array(uc_currency_format($credit->pending_payments), uc_currency_format($credit->pending_payments+$ticket_ar))
+        ));
+      }
+    //}
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="<?php print base_path() . path_to_theme(); ?>/css/invoice.css" media="all" />
+  <?php if($order->billing_zone == '1') { ?>
+  <title>Receipt</title>
+  <?php } else { ?>
   <title><?php print $profile->title ?> (<?php print $profile->name ?>)</title>
+  <?php } ?>
   <style type="text/css" media="all">
   body {
     -webkit-print-color-adjust:exact;
@@ -42,59 +100,66 @@
 </head>
 <body>
   <div class="invoice">
-    <div class="header">
-      <div class="logo column"></div>
-      <div class="address column">
-        <p>8000 Harwin dr. Suite #200 Houston, TX 77036</p>
-        <p><span class="label">Email: </span>info@general-goods.com</p>
-        <p><span class="label">Website: </span>info@general-goods.com</p>
-        <p><span class="label">Phone: </span>713-780-3636 <span class="label">Fax: </span>713-780-1718</p>
-        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span>93044639</p><?php endif; ?>
+    <?php if($order->billing_zone == '0') { ?>
+      <div class="header">
+        <div class="logo column"></div>
+        <div class="address column">
+          <p>8000 Harwin dr. Suite #200 Houston, TX 77036</p>
+          <p><span class="label">Email: </span>info@general-goods.com</p>
+          <p><span class="label">Website: </span>info@general-goods.com</p>
+          <p><span class="label">Phone: </span>713-780-3636 <span class="label">Fax: </span>713-780-1718</p>
+          <br /><br />
+        </div>
+        <div class="invoice-info column">
+          <?php if($status_names[$order_status]) : ?><p class="invoiceid" style="font-size: 1.5em;"><span class="label"><?php print $status_names[$order_status] ?></span></p><?php endif; ?>
+          <p class="invoiceid"><span class="label">Invoice #:</span> <span class="value"><?php echo $order_link; ?></span></p>
+          <p class="invoicedate"><span class="label">Invoice Date:</span> <span class="value"><?php echo date("n/j/Y g:ia", $order->created); ?></span></p>
+          <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
+          <?php if($profile->field_tax_id[0]['value']) : ?><p><span class="label">Tax ID: </span><?php echo $profile->field_tax_id[0]['value']; ?></p><?php endif; ?>
+        </div>
       </div>
-      <div class="invoice-info column">
-        <!--<p class="page"><span class="label">Page:</span> <span class="value"></span></p>-->
-        <p class="invoiceid"><span class="label">Invoice #:</span> <span class="value"><?php echo $order_link; ?></span></p>
-        <p class="invoicedate"><span class="label">Invoice Date:</span> <span class="value"><?php echo date("n/j/Y g:ia", $order->created); ?></span></p>
-        <?php if($profile->field_tobacco_permit_id[0]['value']) : ?><p><span class="label">Tobacco ID: </span><?php echo $profile->field_tobacco_permit_id[0]['value']; ?></p><?php endif; ?>
-      </div>
-    </div>
 
-    <div class="customer-info box">
-      <div class="header-line"></div>
-      <div class="billing-address cust-address">
-        <span class="label">Sold To:</span>
-        <span class="value">
-          <?php if($uid) { ?>
-          <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
-          <?php print $profile->field_contact_remarks[0]['value'] ?><br />
-          <?php print $profile->field_company_address[0]['street1'] ?><br />
-          <?php print $profile->field_company_address[0]['city'] ?>, <?php print $profile->field_company_address[0]['state'] ?> <?php print $profile->field_company_address[0]['zip'] ?><br />
-          <?php } else { ?>
-            Walk-in Customer
-          <?php } ?>
-        </span>
+      <div class="customer-info box">
+        <div class="header-line"></div>
+        <div class="billing-address cust-address">
+          <span class="label">Sold To:</span>
+          <span class="value">
+            <?php if($uid) { ?>
+            <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
+            <!-- <?php //print $contact ?><br /> -->
+            <?php print $profile->field_company_address[0]['street1'] ?><br />
+            <?php print $profile->field_company_address[0]['city'] ?>, <?php print $profile->field_company_address[0]['state'] ?> <?php print $profile->field_company_address[0]['zip'] ?><br />
+            <?php } else { ?>
+              Walk-in Customer
+            <?php } ?>
+          </span>
+        </div>
+        <?php if($uid) : ?>
+        <div class="shipping-address cust-address">
+          <span class="label">Deliver To:</span>
+          <span class="value">
+            <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
+            <?php print $profile->field_shipping_address[0]['value'] ?><br />
+            <?php print $profile->field_shipping_address[0]['street1'] ?><br />
+            <?php print $profile->field_shipping_address[0]['city'] ?>, <?php print $profile->field_shipping_address [0]['state'] ?> <?php print $profile->field_shipping_address [0]['zip'] ?><br />
+            <!-- <?php //print $contact ?><br /><br /> -->
+            <strong>Phone: </strong><?php print $profile->field_profile_company_phone[0]['number'] ?>
+          </span>
+        </div>
+        <?php endif; ?>
       </div>
-      <?php if($uid) : ?>
-      <div class="shipping-address cust-address">
-        <span class="label">Deliver To:</span>
-        <span class="value">
-          <?php print $profile->title ?> (<?php print $profile->name ?>)<br />
-          <?php print $profile->field_shipping_address[0]['value'] ?><br />
-          <?php print $profile->field_shipping_address[0]['street1'] ?><br />
-          <?php print $profile->field_shipping_address[0]['city'] ?>, <?php print $profile->field_shipping_address [0]['state'] ?> <?php print $profile->field_shipping_address [0]['zip'] ?><br />
-          <?php print $profile->field_contact_remarks[0]['value'] ?><br /><br />
-          <strong>Phone: </strong><?php print $profile->field_profile_company_phone[0]['number'] ?>
-        </span>
-      </div>
-      <?php endif; ?>
-    </div>
+    <?php } ?>
 
     <div class="products">
       <?php
       $tables = array();
-      //Add suggested
-      $header = array(array('data' => 'Carton Barcode', 'class' => 'left-item'), 'Description', 'Packaging', 'Qty', 'Price', 'Extended', 'Unit Price', 'Sugg. Retail', 'Profit Margin', 'Extended Retail');
-
+      if($rma_ticket) {
+        //Add suggested
+        $header = array(array('data' => 'Barcode', 'class' => 'left-item'), 'Description', 'Packaging', 'Qty', 'Price', 'Extended');
+      } else {
+        //Add suggested
+        $header = array(array('data' => 'Barcode', 'class' => 'left-item'), 'Description', 'Packaging', 'Qty', 'Price', 'Extended', 'Unit Price', 'Sugg. Retail', 'Profit Margin', 'Extended Retail');
+      }
       //Condense multi-line item products.
       $exists = array();
       $new = array();
@@ -125,7 +190,7 @@
           if($term->vid == 7) {
             $term_info = db_fetch_object(db_query("SELECT invoice_category, retail_markup FROM {ggw_state_reporting_terms} WHERE vid = '%d'", $term->tid));
             $category = $term_info->invoice_category;
-            $retail_markup = 1+($term_info->retail_markup/100);
+            $retail_markup = $term_info->retail_markup;
             break;
           }
         }
@@ -144,6 +209,9 @@
           $i = 0;
           $tables[$category]['profit'] = 0;
           $tables[$category]['retail'] = 0;
+          if(!isset($tables[$category]['weight'])) {
+            $tables[$category]['weight'] = db_result(db_query("SELECT weight FROM term_data WHERE name = '%s' AND vid = '1'", $category));
+          }
         }
 
         $form = (int)$node->field_prod_form[0]['value'];
@@ -151,25 +219,38 @@
           $form = 1;
         }
         $qty_split = db_result(db_query("SELECT qty_split FROM pos_api_expose_manager_override_log WHERE product_nid = '%d'", $product->order_product_id));
-      	if($qty_split) {
-      		$form = $form/$qty_split;
-      	}
-	      $unit_price = $product->price/$form;
-        $suggested = $unit_price*$retail_markup;
-        $extended = $product->price*$product->qty*$retail_markup;
-        $row = array(
-          $product->model,
-          //$node->field_prod_unit_barcode[0]['value'],
-          $node->title,
-          $node->field_prod_packing[0]['value'],
-          array('data' => round($product->qty, 2), 'class' => 'qty-item'),
-          array('data' => number_format($product->price, 2), 'class' => 'numeric-item'),
-          array('data' => number_format($product->price*$product->qty, 2), 'class' => 'numeric-item'),
-          array('data' => number_format($unit_price, 2), 'class' => 'numeric-item'),
-          array('data' => number_format($suggested, 2), 'class' => 'numeric-item'),
-          array('data' => $term_info->retail_markup.'%', 'class' => 'numeric-item'),
-          array('data' => number_format($extended, 2), 'class' => 'numeric-item')
-        );
+        if($qty_split) {
+          $form = $form/$qty_split;
+        }
+        $unit_price = $product->price/$form;
+        $suggested = ($unit_price/(100-$retail_markup))*100;
+        $extended = (($product->price*$product->qty)/(100-$retail_markup))*100;
+
+        if($rma_ticket) {
+          $row = array(
+            $product->model,
+            //$node->field_prod_unit_barcode[0]['value'],
+            strlen($node->title) > $title_limit ? substr($node->title,0,$title_limit)."..." : $node->title,
+            strlen($node->field_prod_packing[0]['value']) > $packaging_limit ? substr($node->field_prod_packing[0]['value'],0,$packaging_limit)."..." : $node->field_prod_packing[0]['value'],
+            array('data' => round($product->qty, 2), 'class' => 'qty-item'),
+            array('data' => number_format($product->price, 2), 'class' => 'numeric-item'),
+            array('data' => number_format($product->price*$product->qty, 2), 'class' => 'numeric-item')
+          );
+        } else {
+          $row = array(
+            $product->model,
+            //$node->field_prod_unit_barcode[0]['value'],
+            strlen($node->title) > $title_limit ? substr($node->title,0,$title_limit)."..." : $node->title,
+            strlen($node->field_prod_packing[0]['value']) > $packaging_limit ? substr($node->field_prod_packing[0]['value'],0,$packaging_limit)."..." : $node->field_prod_packing[0]['value'],
+            array('data' => round($product->qty, 2), 'class' => 'qty-item'),
+            array('data' => number_format($product->price, 2), 'class' => 'numeric-item'),
+            array('data' => number_format($product->price*$product->qty, 2), 'class' => 'numeric-item'),
+            array('data' => number_format($unit_price, 2), 'class' => 'numeric-item'),
+            array('data' => number_format($suggested, 2), 'class' => 'numeric-item'),
+            array('data' => $term_info->retail_markup.'%', 'class' => 'numeric-item'),
+            array('data' => number_format($extended, 2), 'class' => 'numeric-item')
+          );
+        }
 
         $tables[$category]['rows'][] = $row;
         $tables[$category]['line_count']++;
@@ -181,30 +262,75 @@
         $i++;
         $tables[$category]['retail'] += $extended;
       }
+
+      uasort($tables, 'sortCategoryCallback');
+      $j = 0;
+      $cat_count = count($tables);
       foreach($tables as $category => $rows) {
         usort($rows['rows'], 'sortCallback');
-        print '<div class="category-wrap">'.$category.'</div>';
-        $rows['rows'][] = array(
-          '',
-          '<center><strong>Line Count:</strong> '.$tables[$category]['line_count'].'</center>',
-          array('data' => '<div style="text-align: right;"><strong>Count:</strong></div>', 'class' => 'no-border'),
-          '<center>'.$tables[$category]['item_count'].'</center>',
-          '',
-          array('data' => '<strong>$'.number_format($tables[$category]['total'], 2).'</strong>', 'class' => 'numeric-item'),
-          '',
-          '',
-          //array('data' => '<strong>$'.number_format($tables[$category]['unit_price'], 2).'</strong>', 'class' => 'numeric-item'),
-          //array('data' => '<strong>$'.number_format($tables[$category]['suggested'], 2).'</strong>', 'class' => 'numeric-item'),
-          array('data' => '<strong>'.number_format($tables[$category]['profit']/$i, 2).'%</strong>', 'class' => 'numeric-item'),
-          array('data' => '<strong>$'.number_format($tables[$category]['retail'], 2).'</strong>', 'class' => 'numeric-item')
-        );
-        print theme('table', $header, $rows['rows']);
+
+        if($cat_count > 1 && $j == $cat_count-1) {
+          print '<div style="page-break-inside: avoid;">';
+        } else {
+          print '<div>';
+        }
+
+        print '<div class="category-wrap" style="page-break-inside: avoid;">'.$category.'</div>';
+        if($rma_ticket) {
+          $rows['rows'][] = array(
+            '',
+            '<center><strong>Line Count:</strong> '.$tables[$category]['line_count'].'</center>',
+            array('data' => '<div style="text-align: right;"><strong>Count:</strong></div>', 'class' => 'no-border'),
+            '<center>'.$tables[$category]['item_count'].'</center>',
+            '',
+            array('data' => '<strong>$'.number_format($tables[$category]['total'], 2).'</strong>', 'class' => 'numeric-item')
+          );
+        } else {
+          $rows['rows'][] = array(
+            '',
+            '<center><strong>Line Count:</strong> '.$tables[$category]['line_count'].'</center>',
+            array('data' => '<div style="text-align: right;"><strong>Count:</strong></div>', 'class' => 'no-border'),
+            '<center>'.$tables[$category]['item_count'].'</center>',
+            '',
+            array('data' => '<strong>$'.number_format($tables[$category]['total'], 2).'</strong>', 'class' => 'numeric-item'),
+            '',
+            '',
+            //array('data' => '<strong>$'.number_format($tables[$category]['unit_price'], 2).'</strong>', 'class' => 'numeric-item'),
+            //array('data' => '<strong>$'.number_format($tables[$category]['suggested'], 2).'</strong>', 'class' => 'numeric-item'),
+            array('data' => '<strong>'.number_format($tables[$category]['profit']/$tables[$category]['line_count'], 2).'%</strong>', 'class' => 'numeric-item'),
+            array('data' => '<strong>$'.number_format($tables[$category]['retail'], 2).'</strong>', 'class' => 'numeric-item')
+          );
+        }
+
+        print theme('table', $header, $rows['rows']).'</div>';
+        $j++;
       }
       ?>
+        <div class="last-item" style="page-break-inside: avoid;">
+          <hr style="margin-top: 5em;" />
+          <?php if($rma_ticket) { ?>
+          <div class="line-items"><?php print str_replace('Total', 'Refund Total', uc_order_pane_line_items('view', $order)); ?></div>
+          <?php } else { ?>
+          <div class="line-items"><?php print uc_order_pane_line_items('view', $order); ?></div>
+          <?php } ?>
+            <hr />
+            <?php if($order->billing_zone == '0') { ?>
+            <div class="payment-details"><?php print ggw_backend_transaction_details($order->order_id); ?></div>
+            <?php } else { ?>
+            <div class="payment-details"><?php print ggw_backend_transaction_details_dp($order->order_id); ?></div>
+            <?php } ?>
+            <?php if(isset($payment_remaining)) : ?>
+            <hr />
+            <div class="payment-details">
+                  <p><?php print $payment_remaining; ?></p>
+                  <hr />
+            </div>
+            <?php endif; ?>
+          </div>
+          <?php if(!$rma_ticket) : ?>
+            <p style="margin: 2pt 0 0;">Customer Signature _____________________</p>
+          <?php endif; ?>
+        </div>
     </div>
-
-    <div class="line-items"><?php print uc_order_pane_line_items('view', $order); ?></div>
-    <div class="payment-details"><?php print ggw_backend_transaction_details($order->order_id); ?></div>
-  </div>
 </body>
 </html>
